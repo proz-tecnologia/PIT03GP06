90541f611f538cd89dab3467a28795eb7bfd51db
polyana patricia da silva <patriciapolyana34@gmail.com>
2022-10-31 14:54:40 -0300

    alteração do padding em:balances,dicas,categories e spending.dart

--- lib/src/features/home/widgets/bodywidgets/balances.dart
+++ lib/src/features/home/widgets/bodywidgets/balances.dart
@@ -20,7 +20,7 @@ class _BalancesState extends State<Balances> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 16, left: 12, right: 12),
+      padding: const EdgeInsets.only(top: 14, left: 16, right: 16),
       child: AnimatedBuilder(
           animation: DarkController.instance,
           builder: (BuildContext context, Widget? child) {
