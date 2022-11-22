bd9fe14fa5c8711247405c16af7c542a5be8c70c
Paulo Cesar <pc.uai42@outlook.com>
2022-11-22 12:58:32 -0300

    src/controller/history_page_controll, src/home/widgets/piechart, src/registers/pages/despesas

--- lib/src/controllers/providercontrolers/history_page_controller.dart
+++ lib/src/controllers/providercontrolers/history_page_controller.dart
@@ -2,8 +2,9 @@ import 'package:ctrl_real/src/model/transections_model.dart';
 import 'package:flutter/cupertino.dart';
 
 class HistoryController extends ChangeNotifier {
+
   final List<Transaction> transactionList = [];
-  double saldo = 10000;
+  double saldo = 5000;
   double supermerc = 0;
   double lazer = 0;
   double transpor = 0;
@@ -11,9 +12,30 @@ class HistoryController extends ChangeNotifier {
   double pagament = 0;
   double farmac = 0;
 
-  void setTransAction(Transaction trans) {
+  setTransAction(Transaction trans) {
     transactionList.add(trans);
     notifyListeners();
+  }
+
+  double porcentSupermerc(double result){
+    return result = supermerc * 100 / saldo;
+  }
+
+  double porcentLazer(double result){
+    return result = lazer * 100 / saldo;
+  }
+
+  double porcentTranspor(double result){
+    return result = transpor * 100 / saldo;
+  }
+    double porcentGastosex(double result){
+    return result = gastosex * 100 / saldo;
+  }
+    double porcentPagament(double result){
+    return result = pagament * 100 / saldo;
+  }
+    double porcentFarmac(double result){
+    return result = farmac * 100 / saldo;
   }
 
 }
