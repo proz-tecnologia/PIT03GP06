3fbd2dc5b107356f462461fa71efe2031e87d214
AngelOttoni <angel_m_ottoni@hotmail.com>
2023-01-10 19:51:32 -0300

    tela de edição das notificações implementada

---
+++ lib/src/controllers/notifications/button_switch_controller.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SwitchOnOff extends StatefulWidget {
+  const SwitchOnOff({super.key});
+
+  @override
+  State<SwitchOnOff> createState() => _SwitchOnOffState();
+}
+
+class _SwitchOnOffState extends State<SwitchOnOff> {
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      // This bool value toggles the switch.
+      value: light,
+      activeColor: Colors.deepPurple,
+      onChanged: (bool value) {
+        // This is called when the user toggles the switch.
+        setState(() {
+          light = value;
+        });
+      },
+    );
+  }
+}
