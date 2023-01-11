7d9d9fc181983f0f4a5d99adf56326f11944b2cd
AngelOttoni <angel_m_ottoni@hotmail.com>
2023-01-10 22:25:49 -0300

    provider implementado

--- lib/src/view/settings/pages/edit_notifications_page.dart
+++ lib/src/view/settings/pages/edit_notifications_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ctrl_real/src/view/settings/widgets/button_switch.dart';
-import 'package:provider/provider.dart';
-
-import '../../../controllers/notifications/button_switch_controller.dart';
 
 class EditNotificationsPage extends StatelessWidget {
   const EditNotificationsPage({super.key});
@@ -14,21 +11,18 @@ class EditNotificationsPage extends StatelessWidget {
         title: const Text('Editar notificações'),
         centerTitle: true,
       ),
-      body: ChangeNotifierProvider<ButtonSwitchController>(
-        create: (context) => ButtonSwitchController(),
-        child: ListView(children: [
+      body: ListView(children: const [
           ListTile(
-              title: const Text('Receber notificações'),
+              title: Text('Receber notificações'),
               trailing: SwitchButton()),
           // const ListTile(
           //   title: Text('Frequência diária'),
           //   trailing:
           // ),
           ListTile(
-              title: const Text('Habilitar som'),
+              title: Text('Habilitar som'),
               trailing: SwitchButton()),
         ]),
-      ),
-    );
+      );
   }
 }
