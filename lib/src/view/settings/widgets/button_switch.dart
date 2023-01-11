db3f4dcd1756a6fc208519b2df03216e99d317ce
AngelOttoni <angel_m_ottoni@hotmail.com>
2023-01-10 22:27:21 -0300

    gerenciamento de estado

--- lib/src/view/settings/widgets/button_switch.dart
+++ lib/src/view/settings/widgets/button_switch.dart
@@ -2,50 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ctrl_real/src/controllers/notifications/button_switch_controller.dart';
 
-class SwitchButton extends StatelessWidget {
-  SwitchButton({super.key});
-  final _controller = ButtonSwitchController();
-
-  //bool isSwitchEnabled = true;
+class SwitchButton extends StatefulWidget {
+  const SwitchButton({super.key});
+  //final _controller = ButtonSwitchController();
 
   @override
+  State<SwitchButton> createState() => _SwitchButtonState();
+}
+
+class _SwitchButtonState extends State<SwitchButton> {
+  final controller = ButtonSwitchController();
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Switch(
-          activeColor: Colors.deepPurple,
-          value: _controller.isNotificationEnabled,
-          onChanged: _controller.changeNotificationState(),
-        ),
-        Opacity(
-          opacity: _controller.isNotificationEnabled ? 0.4 : 1,
-          child: Switch(
+    return ChangeNotifierProvider(
+      create: (context) => controller,
+      builder: (context, child) => ListView(
+        children: [
+          Switch(
             activeColor: Colors.deepPurple,
-            value: _controller.isSoundEnabled,
-            onChanged: _controller.isNotificationEnabled ? null : _controller.changeSoundState(),
+            value: controller.isNotificationEnabled,
+            onChanged: controller.changeNotificationState(),
           ),
-        ),
-      ],
+          Opacity(
+            opacity: controller.isNotificationEnabled ? 0.4 : 1,
+            child: Switch(
+              activeColor: Colors.deepPurple,
+              value: controller.isSoundEnabled,
+              onChanged: controller.isNotificationEnabled
+                  ? null
+                  : controller.changeSoundState(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-// class SwitchButton extends StatelessWidget {
-//   final VoidCallback valueTest;
-//   final bool valueDois;
-//   const SwitchButton(this.valueTest, this.valueDois, {super.key});
-//
-//   //bool isSwitchEnabled = true;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Switch(
-//         // This bool value toggles the switch.
-//         value: valueDois,
-//         activeColor: Colors.deepPurple,
-//         onChanged: (bool value){
-//           valueTest;
-//         },
-//       );
-//   }
-// }
