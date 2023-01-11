4049134dd6da8a831ffc4ecdf26cda6133f04b3e
AngelOttoni <angel_m_ottoni@hotmail.com>
2023-01-10 22:23:39 -0300

    remoção de código antigo

--- lib/src/controllers/notifications/button_switch_controller.dart
+++ lib/src/controllers/notifications/button_switch_controller.dart
@@ -14,27 +14,3 @@ class ButtonSwitchController extends ChangeNotifier {
     notifyListeners();
   }
 }
-
-
-
-
-
-// import 'package:flutter/material.dart';
-//
-// class ButtonSwitchController extends ChangeNotifier {
-//   bool isNotificationEnabled = true;
-//   bool isSoundEnabled = true;
-//
-//   changeNotificationState(value) {
-//     value = !isNotificationEnabled;
-//     notifyListeners();
-//   }
-//
-//   changeButtonSound(value) {
-//     value = !isSoundEnabled;
-//     if (isNotificationEnabled == false) {
-//       isSoundEnabled == false;
-//     }
-//     notifyListeners();
-//   }
-// }
