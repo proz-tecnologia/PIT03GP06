698dfb569b2d7a2f1d5f5a3fbac0923b59c33fc9
AngelOttoni <angel_m_ottoni@hotmail.com>
2023-01-11 22:39:51 -0300

    estilização do botão dropdown

--- lib/src/view/settings/widgets/notifications/notification_buttons.dart
+++ lib/src/view/settings/widgets/notifications/notification_buttons.dart
@@ -8,7 +8,7 @@ class NotificationButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 94,
+      width: 104,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
         child: Text(NotificationPageController.instance.isNotificationEnabled
@@ -29,7 +29,7 @@ class SoundButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<NotificationPageController>(
       builder: (context, value, child) => SizedBox(
-        width: 94,
+        width: 104,
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
           child: Text(NotificationPageController.instance.isSoundEnabled
@@ -44,7 +44,6 @@ class SoundButton extends StatelessWidget {
   }
 }
 
-
 class FrequencyButton extends StatelessWidget {
   final frequencyValue = ValueNotifier('');
   final frequencyOptions = ['01', '02', '03'];
@@ -54,22 +53,40 @@ class FrequencyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      // width: 94,
-
-      child: ValueListenableBuilder(
-          valueListenable: frequencyValue,
-          builder: (BuildContext context, String value, _) {
-            return DropdownButton<String>(
-              hint: const Text('Selecionar'),
-              //dropdownColor: Colors.deepPurple,
-              value: (value.isEmpty) ? null : value,
-              onChanged: (option) => frequencyValue.value = option.toString(),
-              items: frequencyOptions
-                  .map((option) =>
-                      DropdownMenuItem(value: option, child: Text(option)))
-                  .toList(),
-            );
-          }),
+      width: 104,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
+        child: ValueListenableBuilder(
+            valueListenable: frequencyValue,
+            builder: (BuildContext context, String value, _) {
+              return DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  icon: const Icon(Icons.arrow_drop_down_outlined,
+                  color: Colors.white
+                  ),
+                  iconSize: 14,
+                  hint: const Text(
+                    'Selecione',
+                    style: TextStyle(
+                      fontSize: 14,
+                      leadingDistribution: TextLeadingDistribution.proportional,
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  value: (value.isEmpty) ? null : value,
+                  onChanged: (option) =>
+                      frequencyValue.value = option.toString(),
+                  items: frequencyOptions
+                      .map((option) =>
+                          DropdownMenuItem(value: option, child: Text(option)))
+                      .toList(),
+                ),
+              );
+            }),
+        onPressed: () {},
+      ),
     );
   }
 }
