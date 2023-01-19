f783e8c4e85053d487323d34f09bd463f3b219f3
Janiele Justino <janiele.justino.silva@outlook.com>
2023-01-18 21:24:22 -0300

    Atualizando saldo com get

--- lib/src/controllers/transactions_controller.dart
+++ lib/src/controllers/transactions_controller.dart
@@ -151,7 +151,17 @@ class TransactionsController extends ChangeNotifier {
             element.data() as Map<String, dynamic>);
         registersList.add(lista);
       }
+      renda = 0;
+      saida = 0;
+      for (var element in registersList) {
+        if (element.type == "Receita") {
+          renda += element.valor;
+        } else if (element.type == "Despesa") {
+          saida += element.valor;
+        }
+      }
       //notifyListeners();
+
     }
     notifyListeners();
   }
