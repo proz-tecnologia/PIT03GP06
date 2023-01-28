32693251e3b66e1d39721a390250381371b3a0b1
Janiele Justino <janiele.justino.silva@outlook.com>
2023-01-28 20:49:17 -0300

    Corrigindo lint: Deletando arquivo de teste não utilizado

--- lib/src/repositories/auth_repository.dart
+++ lib/src/repositories/auth_repository.dart
@@ -2,8 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ctrl_real/src/model/register_model.dart';
 import 'package:ctrl_real/src/service/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 
 class AuthRepository {
   final FirebaseFirestore datb = FirebaseFirestore.instance;
@@ -85,7 +85,9 @@ class AuthRepository {
     try {
       await _auth.sendPasswordResetEmail(email: email);
     } on FirebaseAuthException catch (e) {
-      print(e);
+      if (kDebugMode) {
+        print(e);
+      }
     }
   }
 
