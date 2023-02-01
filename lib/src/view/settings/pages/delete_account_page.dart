77c2a2401ff68af450fc5270f479d1a54ba5b2a3
AngelOttoni <angel_m_ottoni@hotmail.com>
2023-02-01 11:12:15 -0300

    botão de excluir conta atualizado

--- lib/src/view/settings/pages/delete_account_page.dart
+++ lib/src/view/settings/pages/delete_account_page.dart
@@ -22,64 +22,67 @@ class _DeleteAccountPageState extends State<DeleteAccountPage> {
           centerTitle: true,
           title: const Text("Excluir conta"),
         ),
-        body: Column(children: [
-          Container(
-            color: darkFunctionWidgets(),
-            height: 300,
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InkWell(
-                  borderRadius: BorderRadius.circular(50),
-                  child: Stack(
-                    alignment: AlignmentDirectional.bottomEnd,
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(60),
-                        child: Container(
-                          height: 120,
-                          width: 120,
-                          color: const Color.fromARGB(140, 255, 255, 255),
-                          child: imageFunction(),
-                        ),
-                      )
-                    ],
+        body: Column(
+          children: [
+            Container(
+              color: darkFunctionWidgets(),
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomEnd,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(60),
+                          child: Container(
+                            height: 120,
+                            width: 120,
+                            color: const Color.fromARGB(140, 255, 255, 255),
+                            child: imageFunction(),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Consumer<UsersService>(
-                  builder: (context, value, child) => Text(
-                    style: const TextStyle(color: Colors.white, fontSize: 20),
-                    value.name ?? "",
+                  Consumer<UsersService>(
+                    builder: (context, value, child) => Text(
+                      style: const TextStyle(color: Colors.white, fontSize: 20),
+                      value.name ?? "",
+                    ),
                   ),
-                ),
-                Consumer<UsersService>(
-                  builder: (context, value, child) => Text(
-                    value.email ?? "",
-                    style: const TextStyle(color: Colors.white),
+                  Consumer<UsersService>(
+                    builder: (context, value, child) => Text(
+                      value.email ?? "",
+                      style: const TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-              padding: const EdgeInsets.only(top: 80),
-              child: Container(
-                height: 50,
-                width: 154,
-                decoration: BoxDecoration(
-                  color: Colors.redAccent,
-                  borderRadius: BorderRadius.circular(5),
+            Padding(
+              padding: const EdgeInsets.only(top: 90),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(
+                    Colors.redAccent,
+                  )
                 ),
                 child: const Text(
                   'Excluir conta',
                   style:
-                      TextStyle(color: Colors.white, height: 1.8, fontSize: 20),
+                      TextStyle(color: Colors.white, fontSize: 20),
                   textAlign: TextAlign.center,
                 ),
-              )),
-        ]),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
