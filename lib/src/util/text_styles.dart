1a7ae5b65405a58817e494c507a221c3c9ad0655
Janiele Justino <janiele.justino.silva@outlook.com>
2023-02-03 00:27:04 -0300

    Estilizando as fontes

---
+++ lib/src/util/text_styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static TextStyles? _instance;
+
+  TextStyles._();
+
+  static TextStyles get i {
+    _instance ??= TextStyles._();
+    return _instance!;
+  }
+
+  String get font => 'mplus1';
+
+  TextStyle get textLight =>
+      TextStyle(fontWeight: FontWeight.w300, fontFamily: font);
+  TextStyle get textRegular =>
+      TextStyle(fontWeight: FontWeight.normal, fontFamily: font);
+  TextStyle get textBold =>
+      TextStyle(fontWeight: FontWeight.bold, fontFamily: font);
+}
+
+extension TextStyleExtensions on BuildContext {
+  TextStyles get textStyles => TextStyles.i;
+}
