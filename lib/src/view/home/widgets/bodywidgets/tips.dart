a7ac89a065204b694b424b9b7b56d18ec38d4e0b
Janiele Justino <janiele.justino.silva@outlook.com>
2023-02-08 19:54:50 -0300

    Alterado dica de podcast para curso

--- lib/src/view/home/widgets/bodywidgets/tips.dart
+++ lib/src/view/home/widgets/bodywidgets/tips.dart
@@ -8,7 +8,7 @@ class Tips extends StatelessWidget {
   Tips({super.key});
 
   final Uri _urlPodcast = Uri.parse(
-      'https://open.spotify.com/show/0UR7uvXIwzbEiSLZlrp9Na?si=e5bf11dac7b0490d');
+      'https://plataforma.meubolsoemdia.com.br/?gclid=CjwKCAiArY2fBhB9EiwAWqHK6oWQCBu656VahBn8rKX2XeoFWmKND-hAmPSToo51OK8D28c-CqNFnRoC25gQAvD_BwE');
   final Uri _urlEbook = Uri.parse(
       'https://www.amazon.com.br/Guia-financeiro-para-quem-sozinho-ebook/dp/B01M1OGZKO/ref=sr_1_1?qid=1674125483&refinements=p_lbr_books_series_browse-bin%3AFinan%C3%A7as+pessoais&s=digital-text&sr=1-1');
   final Uri _urlVideo =
@@ -61,7 +61,7 @@ class Tips extends StatelessWidget {
                               },
                               child: Padding(
                                 padding: const EdgeInsets.all(8.0),
-                                child: Image.asset('asset/img/podcast.png'),
+                                child: Image.asset('asset/img/curso.png'),
                               )),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
